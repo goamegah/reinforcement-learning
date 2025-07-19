008e0bfa33730e0830af23ca8573d0adaac0b7f4
BENHADDAD Lamia (SNCF RESEAU / DIRECTIONS TECH ET INDUS NAT / DG IGM DTII IP3M AP-3S) <0204528N@commun.ad.sncf.fr>
2025-07-19 12:59:24 +0200

    Intégration du mode déroulement pas à pas dans main.py

--- src/reinfolearn/main.py
+++ src/reinfolearn/main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import json
 
 from reinfolearn.environment.grid_world import GridWorldMDP
 from reinfolearn.environment.line_world import LineWorldMDP
@@ -62,6 +63,10 @@ def play_policy_step_by_step(env, policy):
 
     print(f"Fin de la partie. Score final : {env.score()}")
 
+def load_policy(filepath):
+    with open(filepath, "r") as f:
+        policy = json.load(f)
+    return policy
 
 def main():
     parser = argparse.ArgumentParser(description="Run RL algorithm")
@@ -84,6 +89,11 @@ def main():
         default="dyna_q_plus",
         #required=True
     )
+    parser.add_argument(
+    "--play",
+    action="store_true",
+    help="Dérouler la politique apprise sans relancer l'apprentissage"
+)
     parser.add_argument("--episodes", type=int, default=3000)
     parser.add_argument("--gamma", type=float, default=0.99)
     parser.add_argument("--alpha", type=float, default=0.1)
@@ -96,101 +106,107 @@ def main():
     out_dir = f"outputs/{args.env}/{args.algo}"
     os.makedirs(out_dir, exist_ok=True)
 
-    if args.algo == "policy_iteration":
-        policy, V, scores = policy_iteration(env, gamma=args.gamma,verbose=True)
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_values(V, f"{out_dir}/values.npy")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        plot_scores(scores, title="Policy Iteration")
-
-    elif args.algo == "value_iteration":
-        policy, V, scores = value_iteration(env, gamma=args.gamma)
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_values(V, f"{out_dir}/values.npy")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        plot_scores(scores, title="Value Iteration")
-
-    elif args.algo == "mc_on":
-        policy, Q, scores = mc_on_policy_first_visit(
-            env, gamma=args.gamma, epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="MC On-Policy")
-
-    elif args.algo == "mc_off":
-        policy, Q, scores = mc_off_policy_control(
-            env, gamma=args.gamma, epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="MC Off-Policy")
-
-    elif args.algo == "mc_es":
-        policy, Q, scores = mc_exploring_starts(
-            env, gamma=args.gamma, epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="MC Exploring Starts")
-
-    elif args.algo == "q_learning":
-        policy, Q, scores = q_learning(
-            env, gamma=args.gamma, alpha=args.alpha,
-            epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="Q-Learning")
-
-    elif args.algo == "sarsa":
-        policy, Q, scores = sarsa(
-            env, gamma=args.gamma, alpha=args.alpha,
-            epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="SARSA")
-
-    elif args.algo == "expected_sarsa":
-        policy, Q, scores = expected_sarsa(
-            env, gamma=args.gamma, alpha=args.alpha,
-            epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="Expected SARSA")
-
-    elif args.algo == "dyna_q":
-        policy, Q, scores = dyna_q(
-            env, gamma=args.gamma, alpha=args.alpha,
-            epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="Dyna-Q")
-
-    elif args.algo == "dyna_q_plus":
-        policy, Q, scores = dyna_q_plus(
-            env, gamma=args.gamma, alpha=args.alpha,
-            epsilon=args.epsilon, nb_episodes=args.episodes
-        )
-        save_policy(policy, f"{out_dir}/policy.json")
-        save_scores(scores, f"{out_dir}/scores.npy")
-        save_q_table(Q, f"{out_dir}/q_table.pkl")
-        plot_scores(scores, title="Dyna-Q+")
-
-    print("[OK] Terminé. Résultats enregistrés dans :", out_dir)
-    play = input("Voulez-vous dérouler la politique apprise pas à pas ? (o/n) ")
-    if play.lower() == 'o':
+    if args.play:
+        try:
+            policy = load_policy(f"{out_dir}/policy.json")
+        except FileNotFoundError:
+            print(f"Politique non trouvée dans {out_dir}. Lancez d'abord l'apprentissage sans --play.")
+            return
         play_policy_step_by_step(env, policy)
+
+    else:
+        if args.algo == "policy_iteration":
+            policy, V, scores = policy_iteration(env, gamma=args.gamma,verbose=True)
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_values(V, f"{out_dir}/values.npy")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            plot_scores(scores, title="Policy Iteration")
+
+        elif args.algo == "value_iteration":
+            policy, V, scores = value_iteration(env, gamma=args.gamma)
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_values(V, f"{out_dir}/values.npy")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            plot_scores(scores, title="Value Iteration")
+
+        elif args.algo == "mc_on":
+            policy, Q, scores = mc_on_policy_first_visit(
+                env, gamma=args.gamma, epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="MC On-Policy")
+
+        elif args.algo == "mc_off":
+            policy, Q, scores = mc_off_policy_control(
+                env, gamma=args.gamma, epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="MC Off-Policy")
+
+        elif args.algo == "mc_es":
+            policy, Q, scores = mc_exploring_starts(
+                env, gamma=args.gamma, epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="MC Exploring Starts")
+
+        elif args.algo == "q_learning":
+            policy, Q, scores = q_learning(
+                env, gamma=args.gamma, alpha=args.alpha,
+                epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="Q-Learning")
+
+        elif args.algo == "sarsa":
+            policy, Q, scores = sarsa(
+                env, gamma=args.gamma, alpha=args.alpha,
+                epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="SARSA")
+
+        elif args.algo == "expected_sarsa":
+            policy, Q, scores = expected_sarsa(
+                env, gamma=args.gamma, alpha=args.alpha,
+                epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="Expected SARSA")
+
+        elif args.algo == "dyna_q":
+            policy, Q, scores = dyna_q(
+                env, gamma=args.gamma, alpha=args.alpha,
+                epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="Dyna-Q")
+
+        elif args.algo == "dyna_q_plus":
+            policy, Q, scores = dyna_q_plus(
+                env, gamma=args.gamma, alpha=args.alpha,
+                epsilon=args.epsilon, nb_episodes=args.episodes
+            )
+            save_policy(policy, f"{out_dir}/policy.json")
+            save_scores(scores, f"{out_dir}/scores.npy")
+            save_q_table(Q, f"{out_dir}/q_table.pkl")
+            plot_scores(scores, title="Dyna-Q+")
+
+        print("[OK] Terminé. Résultats enregistrés dans :", out_dir)
 
 
 
